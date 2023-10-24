192d2337214e3d51dd0b338c26b9ed815eaece4b
DisneyMedina <disney.medina27@gmail.com>
2023-10-24 00:43:09 -0500

    Crud libro completo

--- src/main/java/com/centroinformacion/repository/LibroRepository.java
+++ src/main/java/com/centroinformacion/repository/LibroRepository.java
@@ -3,7 +3,12 @@ package com.centroinformacion.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.centroinformacion.entity.Libro;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LibroRepository extends JpaRepository<Libro, Integer> {
+    @Query("select x from Libro x where x.titulo like ?1")
+    public List<Libro> listaPorNombreLike(String titulo);
 
 }
--- src/main/java/com/centroinformacion/service/LibroService.java
+++ src/main/java/com/centroinformacion/service/LibroService.java
@@ -12,4 +12,6 @@ public interface LibroService {
     public abstract Libro actualiza(Libro libro);
     public abstract List<Libro> listaLibro();
 
+    public abstract List<Libro> listaLibroPorNombreLike(String nombre);
+    public abstract  void eliminaLibro (int idLibro);
 }
