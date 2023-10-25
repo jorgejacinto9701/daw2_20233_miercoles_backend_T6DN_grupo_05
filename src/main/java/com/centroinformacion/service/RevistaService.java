787e95ca8535832c286649f1b8bfdb0d7bf1e274
rreyesc31 <renzopaoloreyescardenas@gmail.com>
2023-10-24 23:31:33 -0500

    CRUD Revista terminado

--- src/main/java/com/centroinformacion/service/RevistaService.java
+++ src/main/java/com/centroinformacion/service/RevistaService.java
@@ -1,9 +1,19 @@
 package com.centroinformacion.service;
 
+import java.util.List;
+
 import com.centroinformacion.entity.Revista;
 
 public interface RevistaService {
 	
 	public Revista registrar(Revista r);
 	
+	public List<Revista> listarTodo();
+	
+	public Revista actualizar(Revista r);
+	
+	public void eliminar(int idRevista);
+	
+	public List<Revista> listarPorNombreLike(String nombre);
+
 }
