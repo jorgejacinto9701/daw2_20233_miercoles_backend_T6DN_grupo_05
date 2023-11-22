d170b08cbb500c7608f8342649d1837553ec5424
rreyesc31 <renzopaoloreyescardenas@gmail.com>
2023-11-22 03:02:26 -0500

    ajustes

--- src/main/java/com/centroinformacion/repository/RevistaRepository.java
+++ src/main/java/com/centroinformacion/repository/RevistaRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.centroinformacion.entity.Revista;
-import com.centroinformacion.entity.Sala;
 
 public interface RevistaRepository extends JpaRepository<Revista, Integer> {
 	
